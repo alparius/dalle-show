da64b6f4d70f53c0ecbb5a2a0834505210c5f369
Luca-vdB <Luca.von.der.brelie@protonmail.com>
2022-08-16 18:18:34 +0200

    added profanity detection

--- backend/prompt_filtering.py
+++ backend/prompt_filtering.py
@@ -1,5 +1,6 @@
-def filter_prompt(prompt):
-    if "WALL-E" in prompt:
-        prompt = prompt.replace("WALL-E", "BMO")
+from better_profanity import profanity
 
+def filter_prompt(prompt):
+    if profanity.contains_profanity(prompt):
+        print("Prompt contains bad language")
     return prompt
