f7a739db3e827abe62d7219c52503e0189aa220c
AIEX-PC <aiex.text2image@protonmail.com>
2022-12-04 21:39:41 +0100

    avatar screens adapted, inactivity reset

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,15 +17,11 @@ const App = () => {
     const [currentPage, setCurrentPage] = useState(Page.Start);
     const [isGerman, setIsGerman] = useState(true);
 
-    const finishPlaying = () => {
-        setCurrentPage(Page.Finish);
-    };
-
     return (
         <IsGermanContext.Provider value={isGerman}>
             <div style={{ width: '1770px', left: '0px', position: 'fixed' }}>
                 {currentPage === Page.Start && <Start setCurrentPage={setCurrentPage} />}
-                {currentPage === Page.Content && <Content finishPlaying={finishPlaying} />}
+                {currentPage === Page.Content && <Content setCurrentPage={setCurrentPage} />}
                 {currentPage === Page.Finish && <Finish setCurrentPage={setCurrentPage} />}
             </div>
             <div style={{ width: '150px', right: '0px', position: 'fixed' }}>
