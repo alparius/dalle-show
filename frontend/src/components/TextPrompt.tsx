bb27897e488617f3bf2fc11b928449d4db693bd8
Alpar Cseke <alpar.cseke@tuebingen.mpg.de>
2022-12-06 11:57:04 +0100

    the input field shall forever be in focus

--- frontend/src/components/TextPrompt.tsx
+++ frontend/src/components/TextPrompt.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, LegacyRef } from 'react';
 import { Form, Input } from 'semantic-ui-react';
 
 type Props = {
@@ -21,11 +21,22 @@ const TextPrompt = ({ enterPressedCallback, disabled, promptText, setPromptText,
         }
     };
 
-    const greyText = (
-        enoughPlaying ?
-            (isGerman ? 'drücken Sie die grüne Taste, um fortzufahren' : 'hit the green button to continue')
-            : (isGerman ? 'drücken Sie die Eingabetaste, um Bilder zu erzeugen' : 'hit Enter to generate images')
-    )
+    // this input field shall never lose focus
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        inputRef.current!.focus();
+    }, []);
+    const handleOnBlur = () => {
+        inputRef.current!.focus();
+    };
+
+    const greyText = enoughPlaying
+        ? isGerman
+            ? 'drücken Sie die grüne Taste, um fortzufahren'
+            : 'hit the green button to continue'
+        : isGerman
+        ? 'drücken Sie die Eingabetaste, um Bilder zu erzeugen'
+        : 'hit Enter to generate images';
 
     return (
         <Form>
@@ -38,13 +49,13 @@ const TextPrompt = ({ enterPressedCallback, disabled, promptText, setPromptText,
                     style={{ place: 'black !important' }}
                     placeholder={isGerman ? 'tippen Sie etwas Ungewöhnliches ein' : 'type in something unusual'}
                     value={promptText}
+                    ref={inputRef as unknown as LegacyRef<Input>}
+                    onBlur={handleOnBlur}
                     onChange={onTextChanged}
                     onKeyPress={handleTextPromptKeyPressed}
                     disabled={disabled}
                 />
-                <label style={{ textAlign: 'right', color: 'grey' }}>
-                    {greyText}
-                </label>
+                <label style={{ textAlign: 'right', color: 'grey' }}>{greyText}</label>
             </Form.Field>
         </Form>
     );
