6ae0ae5c635d9353d10001cabb92aeb2596a3894
AIEX-PC <aiex.text2image@protonmail.com>
2023-01-16 21:22:02 +0100

    smudgy pic cleaned up

--- startup_script.ps1
+++ startup_script.ps1
@@ -1,3 +1,10 @@
+# based on: https://stackoverflow.com/a/41406026
+# go to shell:startup in explorer, create the shortcut
+# Target:       C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+#                   -NoExit -Command "C:\my-script-path\my-script.ps1"
+# Start In:     C:\my-script-path
+
+
 # start backend
 Start-Job -FilePath "C:\Users\text2image\Documents\text2image\dalle-show\startup_script_backend.ps1"
 
